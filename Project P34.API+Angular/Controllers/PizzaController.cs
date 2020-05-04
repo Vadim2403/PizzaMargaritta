f942ef9da8ef5a6adee334e8272702740c95727c
Morgan <Morgan@DESKTOP-UF5I20B>
2020-05-04 19:56:24 +0300

    Pizza tables

---
+++ Project P34.API+Angular/Controllers/PizzaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project_P34.DataAccess;
+
+namespace Project_P34.API_Angular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+       
+    }
+}
---
+++ Project P34.DataAccess/Entity/Pizza.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Project_P34.DataAccess.Entity
+{
+    [Table("tblPizzas")]
+    public class Pizza
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
